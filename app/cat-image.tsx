ae080514468954e6ee30d648f378982d766c62fb
hirata <hirata2024@s.okayama-u.ac.jp>
2026-01-19 21:32:39 +0900

    ビルド時ではなくデプロイ時にAPIキーを読み込むよう変更

--- app/cat-image.tsx
+++ app/cat-image.tsx
@@ -1,17 +1,3 @@
-//// コンポーネントの引数を定義する
-//type CatImageProps = {
-//  url: string;
-//};
-//
-//// 画像を表示するコンポーネント
-//export function CatImage({ url }: CatImageProps) {
-//  return (
-//    <div>
-//      <img src={url} />
-//    </div>
-//  );
-//}
-
 "use client"; // (1) use clientを指定
 
 import { useState } from "react"; // 追加
--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -1,12 +1,16 @@
 "use server";
 
-import { CAT_API_KEY } from "./env"; // 追加
-
 type Image = {
   url: string;
 };
 
 export async function fetchImage(): Promise<Image> {
+  if (!process.env.CAT_API_KEY) {
+    throw new Error("環境変数 CAT_API_KEY が設定されていません");
+  }
+  //console.log("CAT_API_KEYを読み込んだよ");
+
+  const CAT_API_KEY = process.env.CAT_API_KEY;
   const res = await fetch("https://api.thecatapi.com/v1/images/search", {
     headers: { "x-api-key": CAT_API_KEY }, // 追加
   });
